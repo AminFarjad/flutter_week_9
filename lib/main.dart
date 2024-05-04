436834646dc2dc710e01e54ef62bb61e9502b3c8
AminFarjad <amin.farjad.1999@gmail.com>
2024-05-05 02:27:14 +0500

    Added kotlin code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,42 +34,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static const platform = MethodChannel('samples.flutter.dev/battery');
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String _batteryLevel = 'Unknown battery level.';
+
+Future<void> _getBatteryLevel() async {
+  String batteryLevel;
+  try {
+    final result = await platform.invokeMethod<int>('getBatteryLevel');
+    batteryLevel = 'Battery level at $result % .';
+  } on PlatformException catch (e) {
+    batteryLevel = "Failed to get battery level: '${e.message}'.";
   }
+
+  setState(() {
+    _batteryLevel = batteryLevel;
+  });
+}
 
   @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+Widget build(BuildContext context) {
+  return Material(
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            onPressed: _getBatteryLevel,
+            child: const Text('Get Battery Level'),
+          ),
+          Text(_batteryLevel),
+        ],
       ),
-      body: Center(
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-      );
-  }
+    ),
+  );
+}
 }
